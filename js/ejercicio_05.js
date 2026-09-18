48789744015589e82c65c96627ee03c2b3f91044
Antony Sanchez <antonysanchespena572@gmail.com>
2026-09-18 08:18:39 -0500

    Hemos desarrolado nuevos archivos Js

--- js/ejercicio_05.js
+++ js/ejercicio_05.js
@@ -2,10 +2,10 @@ let nombreUsuario = "Pepe"
 let kilometrajeRecorrido = 200;
 let litrosConsumidos = 15;
 
-let consumoPorKolometraje = kilometrajeRecorrido / litrosConsumidos;
+let consumoPorKolometraje = litrosConsumidos / kilometrajeRecorrido;
 
 console.log("Nombre de Usuario; " , nombreUsuario)
 console.log("Kilómetros recorridos: ", kilometrajeRecorrido, "km");
 console.log("Litros consumidos: ", litrosConsumidos, "L");
-console.log("Consumo por kilómetro: ", consumoPorKolometraje.toFixed(2) ,"L/km");
+console.log("Consumo por kilómetro: ", consumoPorKolometraje,"L/km");
 
