51392bde03ba6a17f9e007ed695f119254cf9e70
Antony Sanchez <antonysanchespena572@gmail.com>
2026-09-23 10:59:31 -0500

    he hecho php

---
+++ php/ejercicio_01.php
@@ -0,0 +1,24 @@
+<?php
+// FASE DE ENTRADA:
+// DECLARACION DE VARIABLES Y PREGUNTAS
+// let producto = "Zapatillas adidas"
+
+$producto = "Zapatillas adidas";
+// const curso = "Lógica y progamación"
+$precio = 350;
+$descuento = 0.08;
+$igv = 0.18;
+
+// FASE DE PROCESO : ES LAS LOGICA DEL PROGRAMA
+$precioIGV = $precio * $igv;
+$precioDescuento = $precio * $descuento;
+$precioFinal = $precio + $precioIGV - $precioDescuento;
+
+// FASE DE SALIDA: IMPRESION DE RESULTADO
+// Modo javascrpt: console.log("EL PRECIO DE PRODUCTO ES: " , precio);
+echo "El precio del productoes es: S/" . $precio ."<br>" ;
+echo "El IGV es. S/ " . $precioIGV ."<br>";
+echo "El descuento es: S/" . $precioDescuento ."<br>";
+echo "El precio final es: S/" . $precioFinal ."<br>";
+
+?>
